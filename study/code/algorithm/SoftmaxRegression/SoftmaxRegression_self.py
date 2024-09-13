9550e81a32a01412f54e08344515decd6e627596
AlexWei061 <aty.wei@foxmail.com>
2024-09-13 16:33:00 +0800

    delete show_image

--- study/code/algorithm/SoftmaxRegression/SoftmaxRegression_self.py
+++ study/code/algorithm/SoftmaxRegression/SoftmaxRegression_self.py
@@ -11,15 +11,6 @@ def transform(data, label):                                                     
 mnist_train = gluon.data.vision.FashionMNIST(train = True, transform = transform)
 mnist_test  = gluon.data.vision.FashionMNIST(train = True, transform = transform)
 
-def show_images(images):
-    n = images.shape[0]
-    _, figs = plt.subplots(1, n, figsize = (15, 15))
-    for i in range(n):
-        figs[i].imshow(images[i].reshape((28, 28)).asnumpy())
-        figs[i].axes.get_xaxis().set_visible(False)
-        figs[i].axes.get_yaxis().set_visible(False)
-    plt.show()
-
 def get_text_labels(label):
     text_labels = [ 't-shirt', 'trouser', 'pullover', 'dress', 'coat',
                     'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot' ]
